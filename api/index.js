dc16e1595ea676b19a7501dd20f143dc7b74ff42
sealio-ui <akmalrhmn5@gmail.com>
2025-06-21 14:11:39 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config({ path: __dirname + '/../.env' });
-const connectDB = require('../config/db');
+const connectDB = require('../config/db.js');
 
 const app = express();
 
