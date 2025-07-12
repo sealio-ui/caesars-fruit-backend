942233a7e745709ada45ead54c17f811049e371f
sealio-ui <akmalrhmn5@gmail.com>
2025-07-12 23:19:11 +0700

    Update saleroute.js

--- routes/saleroute.js
+++ routes/saleroute.js
@@ -3,16 +3,9 @@ const router = express.Router();
 const saleController = require('../controllers/salecontrol');
 
 router.get('/', saleController.getSales);
+
 router.post('/', saleController.createSale);
-router.delete('/all', saleController.deleteAllSales); 
-router.delete('/all', saleController.deleteAllSales); 
-router.delete('/sales', async (req, res) => {
-  try {
-    await Sale.deleteMany({});
-    res.json({ message: 'Sales cleared' });
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to clear sales' });
-  }
-});
+
+router.delete('/all', saleController.deleteAllSales);
 
 module.exports = router;
